5211a9a750ecf898979052904e5d146ee7d7eebd
Luke <luke.stephens@trintel.co.za>
2025-02-21 17:19:37 +0200

    small changes

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
     <MainWrapper className="relative bg-quinary overflow-clip">
       {/*<HeroOverlay />*/}
 
-      <div className="flex relative items-center w-full text-center md:text-start py-32 md:h-[700px] ">
-        <div className="h-full justify-between flex flex-col gap-8 z-50 px-3 md:px-8 lg:px-32 2xl:px-0">
+      <div className="flex relative items-center w-full text-center md:text-start py-32 md:h-[700px]">
+        <div className="h-full justify-between flex flex-col gap-16 z-50 px-3 md:px-8 lg:px-32 2xl:px-0">
           <div className="flex flex-col gap-8">
             <HeroChip />
             <HeroMain />
