58487d433c956383e36d0339a8ac7e76c2b2ad15
Luke <luke.stephens@trintel.co.za>
2025-02-21 16:39:06 +0200

    changes for JAX

--- src/components/hero/heroChip.tsx
+++ src/components/hero/heroChip.tsx
@@ -4,7 +4,9 @@ export default function HeroChip() {
   return (
     <div className="p-2 px-4 border border-primary/50 text-primary rounded-full w-fit mx-auto md:ml-0 md: mr-auto flex flex-row gap-2 items-center bg-primary/10">
       <Star size={14} fill="currentColor" />
-      <p className="text-md font-medium">Course start on the 19th March 2025</p>
+      <p className="text-md font-medium">
+        Course starts on the 19th March 2025
+      </p>
     </div>
   );
 }
