914abdc63aeeaf3ffe8b67a2fe76cb785633b7a2
Luke <luke.stephens@trintel.co.za>
2025-02-21 16:52:06 +0200

    test open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   title: "Parenting The Teen Tribe",
   description:
     "As qualified coaches we bring together Parents of Tweens and Teens so that we can build a strong supportive network to navigate these transitional years together.",
+  openGraph: {
+    images: [{ url: "/open-graph.jpg", width: 1200, height: 630 }],
+  },
 };
 
 export default function RootLayout({
